71309fe2e5f598d69c6f6eab390faf943a74afd1
nodes777 <nodes777@gmail.com>
2016-02-11 16:27:27 -0500

    Fixes indentation

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,62 +8,63 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-         it('should have a url', function() {
-            for (var i= 0; i<allFeeds.length; i++){
-            expect(allFeeds[i].url).toBeDefined();
-            expect(allFeeds[i].url.length).not.toBe(0);
+        it('should have a url', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
             }
         });
 
-         it('should have a name', function() {
-            for (var i= 0; i<allFeeds.length; i++){
-            expect(allFeeds[i].name).toBeDefined();
-            expect(allFeeds[i].name.length).not.toBe(0);
+        it('should have a name', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
             }
         });
     });
 
     describe('The menu', function() {
 
-           var bodyClass = function() {
+        var bodyClass = function() {
             return $('body').hasClass('menu-hidden');
-            };
-            var menuInvisibility = bodyClass();//can't pass func into "expect", it must be a var and it must be called
+        };
+        var menuInvisibility = bodyClass(); //can't pass func into "expect", it must be a var and it must be called
 
-         it('should be hidden by default', function(){
+        it('should be hidden by default', function() {
             expect(menuInvisibility).toBe(true);
-         });
+        });
 
-          it('should change when menu icon is clicked', function(){
-           var menuIcon = $('.menu-icon-link');
+        it('should change when menu icon is clicked', function() {
+            var menuIcon = $('.menu-icon-link');
             expect(document.body.classList.contains('menu-hidden')).toBeTruthy();
             menuIcon.click();
             expect(document.body.classList.contains('menu-hidden')).toBeFalsy();
             menuIcon.click();
             expect(document.body.classList.contains('menu-hidden')).toBeTruthy();
-         });
-});
+        });
+    });
     describe('Initial Entries', function() {
-        beforeEach(function(done){
-                loadFeed(0, done);
-            });
-         it('should be in feed container after loadFeed is done', function(){
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('should be in feed container after loadFeed is done', function() {
             var feeds = $('.feed .entry');
             expect(feeds.length).toBeGreaterThan(0);
-         });
+        });
     });
 
     describe('New Feed Selection', function() {
 
-          beforeEach(function(done){
-                loadFeed(0, done);
-            });
-          it('changes content on page', function(){
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+        it('changes content on page', function() {
             var feedChildren = $('.feed').children();
             console.log(feedChildren);
             loadFeed(1);
             var feedChildrenChanged = $('.feed').children();
             expect(feedChildren).not.toBe(feedChildrenChanged);
-         });
+        });
     });
 }());
